8fe32e9c06f5fd9eaa9d7decd48c5882b9b7e178
Maycon <mayconfelipe164@gmail.com>
2021-03-25 12:09:58 -0300

    About view

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,11 +3,19 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
-public class MainViewController implements Initializable{
+public class MainViewController implements Initializable {
 
 	@FXML
 	private MenuItem menuItemSeller;
@@ -15,23 +23,45 @@ public class MainViewController implements Initializable{
 	private MenuItem menuItemDepartment;
 	@FXML
 	private MenuItem menuItemAbout;
-	
+
 	@FXML
 	public void onMenuItemSellerAction() {
 		System.out.println("onMenuItemSellerAction");
 	}
+
 	@FXML
 	public void onMenuItemDepartmentAction() {
 		System.out.println("onMenuItemDepartmentAction");
-	}@FXML
-	public void onMenuItemAboutAction() {
-		System.out.println("onMenuItemAboutAction");
 	}
-	
-	
+
+	@FXML
+	public void onMenuItemAboutAction() {
+		String path = "/gui/About.fxml";
+		loadView(path);
+	}
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		
+
+	}
+
+	private synchronized void loadView(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+
+		} catch (Exception e) {
+			Alerts.showAlert("Exception", "error loading view", e.getMessage(), AlertType.ERROR);
+		}
+
 	}
 
 }
